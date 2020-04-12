584c37dd3d50c06c71191612563146e8f3f9fa7f
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-04-12 15:47:49 +0100

    Update upload-app.sh

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -10,12 +10,17 @@ rm -rf ${TRAVIS_REPO_SLUG:11}*
 
 find ../app/build/outputs -type f -name '*.apk' -exec cp -v {} . \;
 
+git checkout --orphan temporary
+
 for file in app*; do
     if [[ $file != *"unsigned"* ]];then
-        mv $file ${TRAVIS_REPO_SLUG:11}-${file:4}
-        git add ${TRAVIS_REPO_SLUG:11}-${file:4}
-        git commit -am "Update ${TRAVIS_REPO_SLUG:11}-${file:4} ($(date +%Y-%m-%d.%H:%M:%S))"
+        mv $file ${TRAVIS_REPO_SLUG:11}-${file#*-}
+        git add ${TRAVIS_REPO_SLUG:11}-${file#*-}
+        git commit -am "Update ${TRAVIS_REPO_SLUG:11}-${file#*-} ($(date +%Y-%m-%d.%H:%M:%S))"
     fi
 done
 
-git push origin master
+git branch -D master
+git branch -m master
+
+git push origin master --force
